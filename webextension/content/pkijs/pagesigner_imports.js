37b2aa4db0359fe9dbcf3d7fbc123d93a537b79d
themighty1 <themighty1>
2020-10-09 18:11:15 +0300

    add missing files

---
+++ webextension/content/pkijs/pagesigner_imports.js
@@ -0,0 +1,10 @@
+// noinspection JSFileReferences
+import * as asn1js from "./asn1.js";
+// noinspection JSFileReferences
+import Certificate from "./Certificate.js";
+// noinspection JSFileReferences
+import CertificateChainValidationEngine from "./CertificateChainValidationEngine.js";
+
+window.asn1js = asn1js;
+window.Certificate = Certificate;
+window.CertificateChainValidationEngine = CertificateChainValidationEngine;
