a1a9d6485f2d018f34c76ff5d96153bb384bdb72
themighty1 <you@example.com>
2021-11-21 17:34:34 +0300

    remove old files

--- webextension/content/globals.js
+++
@@ -1,5 +0,0 @@
-//globals from main.js cannot be overridden in nodejs unless we put them in a separate file
-var chosen_notary;
-var is_chrome = window.navigator.userAgent.match('Chrome') ? true : false;
-//Future use: use TLS extension when user selects it in options
-var use_max_fragment_length = false;
